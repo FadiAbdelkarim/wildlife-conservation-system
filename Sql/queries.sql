5c8019cad0403d23ed0b81c5e25b1ad0da884247
22310065 <22310065@student.ciu.edu.tr>
2026-05-16 19:38:39 +0300

    Update queries.sql

--- Sql/queries.sql
+++ Sql/queries.sql
@@ -1,24 +1,15 @@
--- =========================================
--- QUERY 1
--- Display all animals
--- =========================================
+
 
 SELECT *
 FROM "Animals";
 
--- =========================================
--- QUERY 2
--- Animals with population greater than 10000
--- =========================================
+
 
 SELECT species, population_estimate
 FROM "Animals"
 WHERE population_estimate > 10000;
 
--- =========================================
--- QUERY 3
--- Join Animals and Sightings
--- =========================================
+
 
 SELECT
     A.species,
@@ -28,10 +19,6 @@ FROM "Animals" A
 JOIN "Sightings" S
 ON A.animal_id = S.animal_id;
 
--- =========================================
--- QUERY 4
--- Count total sightings per animal
--- =========================================
 
 SELECT
     A.species,
@@ -41,10 +28,7 @@ LEFT JOIN "Sightings" S
 ON A.animal_id = S.animal_id
 GROUP BY A.species;
 
--- =========================================
--- QUERY 5
--- Display rangers and their protected areas
--- =========================================
+
 
 SELECT
     R.ranger_name,
@@ -53,19 +37,12 @@ FROM "Rangers" R
 JOIN "ProtectedAreas" P
 ON R.area_id = P.area_id;
 
--- =========================================
--- QUERY 6
--- Average population estimate
--- =========================================
 
 SELECT
     AVG(population_estimate) AS average_population
 FROM "Animals";
 
--- =========================================
--- QUERY 7
--- Protected areas ordered by size
--- =========================================
+
 
 SELECT *
 FROM "ProtectedAreas"
